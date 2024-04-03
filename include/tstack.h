7ea5d3b2b5d27bb71b6d12708d4f8b7abc2ba763
fdlii <155849941+fdlii@users.noreply.github.com>
2024-04-03 20:01:56 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,21 +1,25 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-private:
+ private:
     T data[size];
     int top;
-public:
+
+ public:
     TStack() : top(0) {}
     void push(const T& item) {
         if (top <= size - 1) data[top++] = item;
-        else throw std::string("Full");
+        else 
+            throw std::string("Full");
     }
     T pop() {
         if (top > 0) return data[--top];
-        else throw std::string("Empty");
+        else 
+            throw std::string("Empty");
     }
 
     int getTop() {
