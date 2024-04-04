40173626d2a4817796009302d36c664de5fd98c7
fdlii <155849941+fdlii@users.noreply.github.com>
2024-04-04 15:23:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,7 +10,7 @@ std::string infx2pstfx(std::string inf) {
     for (int i = 0; i < len; i++) {
         std::string var;
         int pr = 0;
-        if (inf[i] > '0' && inf[i] < '9') {
+        if (inf[i] >= '0' && inf[i] <= '9') {
             s += inf[i];
             s += ' ';
         }
@@ -47,32 +47,41 @@ std::string infx2pstfx(std::string inf) {
     }
     while (st.getTop() != 0) {
         s += st.pop();
+        s += ' ';
     }
     return s;
 }
 
 int eval(std::string pref) {
-    TStack<char, 100> st;
+    TStack<int, 100> st;
     int len = pref.length();
     int var1, var2, res;
     for (int i = 0; i < len; i += 2) {
-        if (pref[i] > '0' && pref[i] < '9') {
-            st.push(pref[i]);
+        if (pref[i] >= '0' && pref[i] <= '9') {
+            switch (pref[i]) {
+            case '0': st.push(0); break;
+            case '1': st.push(1); break;
+            case '2': st.push(2); break;
+            case '3': st.push(3); break;
+            case '4': st.push(4); break;
+            case '5': st.push(5); break;
+            case '6': st.push(6); break;
+            case '7': st.push(7); break;
+            case '8': st.push(8); break;
+            case '9': st.push(9); break;
+            }
         }
         else {
-            var1 = st.pop() - '0';
-            var2 = st.pop() - '0';
+            var1 = st.pop();
+            var2 = st.pop();
             switch (pref[i]) {
-            case '+': res = var2 + var1; break;
-            case '-': res = var2 - var1; break;
-            case '*': res = var2 * var1; break;
-            case '/': res = var2 / var1; break;
-            case ' ': res = -1; break;
+                case '+': res = var2 + var1; break;
+                case '-': res = var2 - var1; break;
+                case '*': res = var2 * var1; break;
+                case '/': res = var2 / var1; break;
             }
-            if (res != -1) {
-                st.push((char)(res + 48));
-            }
+            st.push(res);
         }
     }
-    return st.pop() - '0';
+    return st.pop();
 }
